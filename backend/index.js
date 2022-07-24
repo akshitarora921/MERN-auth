3826401b7665d5c7a26ef03aba92b6ae2cc337e8
Akshit <akshitarora9211@gmail.com>
2022-07-24 17:45:30 +0530

    update port number

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import { initializeDbConnection } from "./db.js";
 import { routes } from "./routes/index.js";
 
 const app = express();
-const port = 3000;
+const port = 3030;
 
 app.use(express.json());
 
